13a503708ff343e5c19ac722a6c4c4f52160a6de
Stefan Buck <dev@stefanbuck.com>
2019-05-06 23:14:41 +0200

    Update octolinker version

--- components/demo-header.js
+++ components/demo-header.js
@@ -1,17 +1,16 @@
-const websiteUrl = 'https://octolinker.now.sh';
+const websiteUrl = "https://octolinker.now.sh";
 const chromeUrl =
-  'https://chrome.google.com/webstore/detail/octolinker/jlmafbaeoofdegohdhinkhilhclaklkp';
-const firefoxUrl = 'https://addons.mozilla.org/en-US/firefox/addon/octolinker/';
-const operaUrl = 'https://addons.opera.com/en/extensions/details/octolinker/';
+  "https://chrome.google.com/webstore/detail/octolinker/jlmafbaeoofdegohdhinkhilhclaklkp";
+const firefoxUrl = "https://addons.mozilla.org/en-US/firefox/addon/octolinker/";
+const operaUrl = "https://addons.opera.com/en/extensions/details/octolinker/";
 
 module.exports = `<div class="octolinker-header">
     <div class="inner">
-        <a href="${websiteUrl}">
-            <img src="${websiteUrl}/static/octolinker.png" height="30" alt="OctoLinker – browser extension for GitHub">
+        <a href="${websiteUrl}" class="logo" >
+            <h2>OctoLinker Demo</h2>
         </a>
-        <h2>OctoLinker Demo</h2>
         <div class="cta">
-            <p>Like what you see? Install <a href="${websiteUrl}">OctoLinker</a> now!</p>
+            <p class="hide-on-mobile">Like what you see? Install <a href="${websiteUrl}">OctoLinker</a> now!</p>
             <div class="browser-logos">
                 <a href="${chromeUrl}" rel="nofollow"><img src="/static/chrome.png" width="20"></a>
                 <a href="${firefoxUrl}" rel="nofollow"><img src="/static/firefox.png" width="20"></a>
